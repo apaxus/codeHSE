36b2be319b6ebc742351202ad7396f2537d018aa
apaxus <75012161+apaxus@users.noreply.github.com>
2020-11-25 13:38:08 +0300

    Поменял все

--- CodeHSE.py
+++ CodeHSE.py
@@ -1,3 +1,8 @@
-print("Hello world")
-print("Goodbye world")
-print("Poka")
+a = int(input())
+b = int(input())
+if b > a:
+    for i in range(a, b + 1):
+        print(i, end=" ")
+else:
+    for i in range(a, b-1 , -1):
+        print(i, end=" ")
